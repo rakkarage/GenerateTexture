f9e8585280dda366b4fd26a83eb04de0c760d25b
rakkarage <rakkarage@gmail.com>
2022-02-17 14:19:46 -0500

    x

--- TestGenerate.gd
+++ TestGenerate.gd
@@ -4,7 +4,7 @@ extends Control
 @onready var _generate : Button = $Generate
 @onready var _save : Button = $Save
 
-@export var _size := Vector2i(128, 128)
+@export var _size := 128
 @export var _seedMax := 65535
 @export var _path := "res://TestGenerate.png"
 @export var _noise := OpenSimplexNoise.new()
@@ -12,21 +12,21 @@ extends Control
 var _output := Image.new()
 
 func _ready() -> void:
-	_output.create(_size.x, _size.y, false, Image.FORMAT_RGBA8)
+	_output.create(_size, _size, false, Image.FORMAT_RGBA8)
 	assert(_generate.connect("pressed", _generatePressed) == OK)
 	assert(_save.connect("pressed", _savePressed) == OK)
 	_generatePressed()
 
 func _generatePressed() -> void:
 	_noise.seed += randi() % _seedMax
-	var image1 := _noise.get_image(_size.x, _size.y)
+	var image1 := _noise.get_seamless_image(_size)
 	_noise.seed += randi() % _seedMax
-	var image2 := _noise.get_image(_size.x, _size.y)
+	var image2 := _noise.get_seamless_image(_size)
 	_noise.seed += randi() % _seedMax
-	var image3 := _noise.get_image(_size.x, _size.y)
+	var image3 := _noise.get_seamless_image(_size)
 
-	for y in range(_size.y):
-		for x in range(_size.x):
+	for y in range(_size):
+		for x in range(_size):
 			var r = image1.get_pixel(x, y).r
 			var g = image2.get_pixel(x, y).r
 			var b = image3.get_pixel(x, y).r
